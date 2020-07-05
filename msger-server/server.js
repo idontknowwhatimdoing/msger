f5d8544d383056911710bf5b039919b73b806203
jeremy <jeyx.22@gmail.com>
2020-07-05 15:27:04 +0200

    add server in repo

---
+++ msger-server/server.js
@@ -0,0 +1,21 @@
+const net = require("net");
+const PORT = 1337;
+let clients = [];
+
+const server = net.createServer((client) => {
+	console.log("new connection : ", client.remoteAddress + ":" + client.remotePort);
+	clients.push(client);
+
+	client.on("data", (data) => {
+		console.log("data recieved");
+		for (const c of clients) c.write(data);
+	});
+
+	client.on("end", () => {
+		console.log(client.remoteAddress + ":" + client.remotePort, "disconnected");
+		clients.splice(clients.indexOf(client), 1);
+	});
+});
+
+server.on("error", (err) => console.error(err));
+server.listen(PORT, () => console.log("server listening on port ", PORT, "..."));
