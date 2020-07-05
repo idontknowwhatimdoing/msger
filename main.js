e838be8f839de1d6fc2e3e33a5bad0be89ac8562
jeremy <jeyx.22@gmail.com>
2020-07-05 15:58:19 +0200

    yep cock

--- main.js
+++ main.js
@@ -12,8 +12,7 @@ function createWindow() {
 
 	// and load the index.html of the app.
 	win.loadFile("index.html");
-	win.webContents.openDevTools();
-	// win.removeMenu();
+	win.removeMenu();
 }
 
 // This method will be called when Electron has finished
