6c278cccf8f3e9dd49278d22d66c94107ca71270
jeremy <jeyx.22@gmail.com>
2020-07-06 13:06:56 +0200

    alors peut être ?

--- msger-server/server.js
+++ msger-server/server.js
@@ -3,20 +3,16 @@ const PORT = 1337;
 let clients = [];
 
 const server = net.createServer((client) => {
-	console.log("new connection : ", client.remoteAddress);
+	console.log("new connection : ", client.remoteAddress + ":" + client.remotePort);
 	clients.push(client);
 
 	client.on("data", (data) => {
-		console.log(client.remoteAddress, ":", data.toString());
-		for (const c of clients) {
-			if (c.write(data)) console.log("ok");
-			else console.log("not ok");
-			console.log(c.bytesWritten);
-		}
+		console.log(client.remoteAddress + ":" + client.remotePort, ":", data.toString());
+		for (const c of clients) c.write(data);
 	});
 
 	client.on("end", () => {
-		console.log(client.remoteAddress, "disconnected");
+		console.log(client.remoteAddress + ":" + client.remotePort, "disconnected");
 		clients.splice(clients.indexOf(client), 1);
 	});
 });
