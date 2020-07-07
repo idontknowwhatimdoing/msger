037a2342e21e5ccdb04819d329e7e22355298542
jeremy <jeyx.22@gmail.com>
2020-07-07 19:42:09 +0200

    bruh

--- src/chat.js
+++ src/chat.js
@@ -2,32 +2,35 @@
 
 function display_message(msg) {
 	let msg_div = document.createElement("div");
-	msg_div.innerText = msg.author + " : " + msg.content;
+	// msg_div.innerText = msg.author + " : " + msg.content;
+	msg_div.innerHTML = msg;
 	msg_div.className = "msg";
 	document.body.insertBefore(msg_div, document.getElementById("last_div"));
 }
 
 function send_message(e) {
 	if (e.code === "Enter" && input.value !== "") {
-		let buf = Buffer.from(JSON.stringify({ content: input.value, author: username }));
-		client.write(buf);
+		// let buf = Buffer.from(JSON.stringify({ content: input.value, author: username }));
+		// client.write(buf);
+		client.write(input.value);
 		console.log("writing :", client.bytesWritten);
 		input.value = "";
 	}
 }
 
-const { Buffer } = require("buffer");
+// const { Buffer } = require("buffer");
 const net = require("net");
 const PORT = 1337;
 const HOST = "192.168.0.21";
 const client = net.createConnection(PORT, HOST, () => console.log("client connected"));
 const input = document.getElementById("msg_input");
-let username = sessionStorage.getItem("username");
+// let username = sessionStorage.getItem("username");
 
 input.onkeydown = send_message;
 client.on("data", (data) => {
 	console.log("reading :", client.bytesRead);
-	display_message(JSON.parse(data.toString()));
+	// display_message(JSON.parse(data.toString()));
+	display_message(data.toString());
 });
 client.on("drain", () => console.log("drain event"));
 client.on("error", (e) => console.error("error :", e));
